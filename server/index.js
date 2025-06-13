32e8b62ef9883d47de641bd87c8741d11349a39f
aura80 <aura_dragan80@yahoo.com>
2025-06-13 05:41:40 +0300

    Update CORS and env for Railway

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
+      process.env.FRONTEND_URL,
       "http://localhost:3000",
       "https://aura80.github.io",
       "https://aura80.github.io/slim-mom",

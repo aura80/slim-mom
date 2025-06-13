6dea51544fd087146d11d497db7d10e0bb3ba35a
aura80 <aura80@users.noreply.github.com>
2025-06-13 15:34:03 +0000

    deploy: f1e14f1d4fa0bede526a026c5e7430e99c266075

--- server/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {createProduct, getAllProducts} = require('../controllers/productController');
-const router = express.Router();
-
-router.post("/createProduct", createProduct);
-router.get("/getAllProducts", getAllProducts);
-
-module.exports = router;

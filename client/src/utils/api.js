d648aef3c433fd800b8621e1e222f2b6356e8a07
aura80 <aura_dragan80@yahoo.com>
2025-06-13 08:01:27 +0300

    Update CORS and env for Railway

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,15 +1,17 @@
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
 const API_LOGIN_URL = `${apiUrl}/api/auth`;
 
 export async function loginUser({ email, password }) {
-    const response = await fetch(`${API_LOGIN_URL}/login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+  const response = await fetch(`${API_LOGIN_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
 
-    if (!response.ok) throw new Error('Login failed')
+  if (!response.ok) throw new Error("Login failed");
 
-    return response.json();
+  return response.json();
 }
 
 export async function registerUser({ name, email, password }) {
